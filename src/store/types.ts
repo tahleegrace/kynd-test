9e0c9cfdf3dee670bf33620ff9dc8763449d41f0
Tahlee-Joy Grace <tahlee.grace@gmail.com>
2023-11-22 14:18:10 +1000

    Added Member type.

--- src/store/types.ts
+++ src/store/types.ts
@@ -4,3 +4,11 @@ export interface StoreState {
   title: string,
   showingMembers: boolean
 }
+
+export interface Member {
+  firstName: string;
+  lastName: string;
+  dateOfBirth: Date;
+  dateOfJoining: Date;
+  description: string;
+}
